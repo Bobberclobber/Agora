23b71cf06b538a127e2e9ce6e4b6339aca567703
Josef Fagerström <josef.fagerstrom@hotmail.com>
2014-10-10 15:11:52 +0200

    Added new intial (home) activity. Added action bar to the home activity

---
+++ Project/project/src/main/java/se/liu/ida/josfa969/tddd80/activities_v2/HomeActivity.java
@@ -0,0 +1,39 @@
+package se.liu.ida.josfa969.tddd80.activities_v2;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import se.liu.ida.josfa969.tddd80.R;
+
+public class HomeActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        ActionBar actionBar = getActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayShowTitleEnabled(false);
+        }
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
+    }
+}
