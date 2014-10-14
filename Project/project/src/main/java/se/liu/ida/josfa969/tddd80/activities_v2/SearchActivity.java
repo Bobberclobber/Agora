7a5e71b7bb287bc1178fcffe9150a7ba4fd56c3d
Bobberclobber <josef.fagerstrom@hotmail.com>
2014-10-14 23:37:53 +0200

    Added all search options to the search activity's layout

--- Project/project/src/main/java/se/liu/ida/josfa969/tddd80/activities_v2/SearchActivity.java
+++ Project/project/src/main/java/se/liu/ida/josfa969/tddd80/activities_v2/SearchActivity.java
@@ -1,24 +1,26 @@
 package se.liu.ida.josfa969.tddd80.activities_v2;
 
 import android.app.ActionBar;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import se.liu.ida.josfa969.tddd80.R;
-import se.liu.ida.josfa969.tddd80.fragments_v2.HomeFragment;
+import se.liu.ida.josfa969.tddd80.fragments_v2.SearchFragment;
 
-public class HomeActivity extends ActionBarActivity {
+public class SearchActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_search);
 
+        // Adds the fragment to the screen
         if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction().add(R.id.container, new HomeFragment()).commit();
+            getFragmentManager().beginTransaction().add(R.id.container, new SearchFragment()).commit();
         }
     }
 
@@ -35,15 +37,26 @@ public class HomeActivity extends ActionBarActivity {
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Populates the tag spinner
+        Spinner tagSpinner = (Spinner) findViewById(R.id.tag_search_spinner);
+        // Create an ArrayAdapter using the searchable tags array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.tags_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        tagSpinner.setAdapter(adapter);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         switch (item.getItemId()) {
-            case R.id.menu_action_search:
-                Intent searchActivityIntent = new Intent(this, SearchActivity.class);
-                startActivity(searchActivityIntent);
-                return true;
             case R.id.menu_action_settings:
                 return true;
             default:
---
+++ Project/project/src/main/java/se/liu/ida/josfa969/tddd80/fragments_v2/HomeFragment.java
@@ -0,0 +1,22 @@
+package se.liu.ida.josfa969.tddd80.fragments_v2;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import se.liu.ida.josfa969.tddd80.R;
+
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+}
